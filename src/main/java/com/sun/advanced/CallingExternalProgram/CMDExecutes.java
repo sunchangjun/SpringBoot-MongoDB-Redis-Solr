48896b4422cedda576ccaffe15764f2ba5451d41
sun <sunchangjunn@outlook.com>
2018-11-23 15:45:31 +0800

    监控服务器

--- src/main/java/com/sun/advanced/CallingExternalProgram/CMDExecutes.java
+++ src/main/java/com/sun/advanced/CallingExternalProgram/CMDExecutes.java
@@ -7,7 +7,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 
 import com.alibaba.fastjson.JSONObject;
@@ -18,10 +21,44 @@ import com.alibaba.fastjson.JSONObject;
  * 2018年11月23日上午11:29:14
  */
 public class CMDExecutes {
-//	@Test
+	@Test
+	public void test3() {
+		System.out.println(executeCmd("cmd /c java"));;
+		System.out.println("执行完毕");
+	}
+	
+	
+	private Map<String, String> executeCmd(String cmd) {
+        Runtime rt = Runtime.getRuntime(); // 运行时系统获取
+        Map<String, String> lineMap = new HashMap<String, String>();//存放返回值
+        try {
+            Process proc = rt.exec(cmd);// 执行命令
+            InputStream stderr = proc.getInputStream();//执行结果 得到进程的标准输出信息流
+            InputStreamReader isr = new InputStreamReader(stderr);//将字节流转化成字符流
+            BufferedReader br = new BufferedReader(isr);//将字符流以缓存的形式一行一行输出
+            String line = null;
+            while ((line = br.readLine()) != null) { 
+                if (!StringUtils.isEmpty(line)) {
+                    String[] strLine = line.split(":");
+                    if(strLine.length>=2) {
+                    	System.out.println(strLine[1].trim());
+                        lineMap.put(strLine[0].trim(), strLine[1].trim());
+                    }
+                    
+                }
+            }
+            br.close();
+            isr.close();
+            stderr.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return lineMap;
+    }
+
 	public void test() throws Exception {
-		Process p = Runtime.getRuntime().exec("cmd.exe /c dir");
-		
+		Process p = Runtime.getRuntime().exec("systeminfo");
+		Thread.sleep(3000);
 		int status = p.waitFor();
 		System.out.println(status);
 		print(p);
@@ -50,7 +87,7 @@ public class CMDExecutes {
 	
 	}
 	
-	@Test
+	
 	public  void test2() {
 	
 //	        String command1="cmd /c net start mysql";
--- src/test/java/com/sun/TestOne.java
+++ src/test/java/com/sun/TestOne.java
@@ -4,24 +4,54 @@
 package com.sun;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryMXBean;
+import java.lang.management.MemoryUsage;
 
 import org.junit.Test;
 
 import com.alibaba.fastjson.JSONObject;
 import com.sun.util.Shell;
-
+import java.io.File;
+import java.lang.management.ManagementFactory;
+import com.sun.management.OperatingSystemMXBean;
 /**
  * @author sunchangjunn 2018年11月23日上午10:45:15
  */
 public class TestOne {
 	@Test
 	public void test() {
-		Shell shell=new Shell("47.98.153.144","root","Scj19890606");
-		System.out.println(shell.execute("free -m"));
+
+		 getMemInfo();
+	        System.out.println();
+	        getDiskInfo();
 	}
+	
+
+	    
+	    public static void getDiskInfo()
+	    {
+	        File[] disks = File.listRoots();
+	        for(File file : disks)
+	        {
+	            System.out.print(file.getPath() + "    ");
+	            System.out.print("空闲未使用 = " + file.getFreeSpace() / 1024 / 1024 + "M" + "    ");// 空闲空间
+	            System.out.print("已经使用 = " + file.getUsableSpace() / 1024 / 1024 + "M" + "    ");// 可用空间
+	            System.out.print("总容量 = " + file.getTotalSpace() / 1024 / 1024 + "M" + "    ");// 总空间
+	            System.out.println();
+	        }
+	    }
+	    
+	    public static void getMemInfo()
+	    {
+	        OperatingSystemMXBean mem = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
+	        System.out.println("Total RAM：" + mem.getTotalPhysicalMemorySize() / 1024 / 1024 + "MB");
+	        System.out.println("Available　RAM：" + mem.getFreePhysicalMemorySize() / 1024 / 1024 + "MB");
+	    }
 
 	
 
